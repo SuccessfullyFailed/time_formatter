b4de9905507fe29914d2d92438e913c3efa14abb
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-10-27 23:24:21 +0100

    Add daylight savings system

--- src/time_formatter.rs
+++ src/time_formatter.rs
@@ -1,17 +1,17 @@
-use std::{ sync::{ OnceLock }, time::{ Duration, SystemTime, UNIX_EPOCH } };
+use std::{ fmt::{Debug, Display}, sync::OnceLock, time::{ Duration, SystemTime, UNIX_EPOCH } };
 
 
 
 pub(crate) const SECONDS_PER_MINUTE:u64 = 60;
 pub(crate) const SECONDS_PER_HOUR:u64 = 60 * SECONDS_PER_MINUTE;
 pub(crate) const SECONDS_PER_DAY:u64 = 24 * SECONDS_PER_HOUR;
-pub(crate) const SECONDS_PER_MONTH:fn(month_index:u64, is_leap_year:bool) -> u64 = |month_index, is_leap_year| if month_index == 1 { if is_leap_year { 28 } else { 29 } } else { 30 + ((month_index + (if month_index > 6 { 0 } else { 1 })) % 2) } * SECONDS_PER_DAY;
+pub(crate) const SECONDS_PER_MONTH:fn(month_index:u64, is_leap_year:bool) -> u64 = |month_index, is_leap_year| if month_index == 1 { if is_leap_year { 29 } else { 28 } } else { 30 + ((month_index + (if month_index > 6 { 0 } else { 1 })) % 2) } * SECONDS_PER_DAY;
 pub(crate) const SECONDS_PER_YEAR:fn(is_leap_year:bool) -> u64 = |is_leap_year| if is_leap_year { 366 } else { 365 } * SECONDS_PER_DAY;
 
 
 
 pub struct TimeFormatter {
-	_seconds_since_y0:u64,
+	seconds_since_y0:u64,
 	year:u16,
 	months:u8,
 	days:u8,
@@ -23,42 +23,71 @@ impl TimeFormatter {
 
 	/* CONSTRUCTOR METHODS */
 
-	/// Create a new formatter from a SystemTime.
+	/// Create a new formatter from a supported time-format.
 	pub fn new<T:TimeFormattable>(time:T) -> TimeFormatter {
 		let seconds_since_y0:u64 = time.seconds_since_y0();
-		let mut remaining_seconds:u64 = seconds_since_y0;
+		let mut time:TimeFormatter = TimeFormatter { seconds_since_y0, year: 0, months: 0, days: 0, hours: 0, minutes: 0, seconds: 0 };
+		time.parse_formatted();
+		time
+	}
 
-		let mut year:u64 = 0;
-		while Self::take_formatted_time(&mut remaining_seconds, SECONDS_PER_YEAR(year % 4 == 0)) {
-			year += 1;
+	/// Create a new formatter from separate date parts. Month and date should start at 1.
+	pub fn new_date(year:u16, month:u8, date:u8, hours:u8, minutes:u8, seconds:u8) -> TimeFormatter {
+		TimeFormatter::new_date_raw(year, month - 1, date - 1, hours, minutes, seconds)
+	}
+
+	/// Return self with daylight saving time enabled.
+	pub fn with_daylight_saving_time(mut self, start_month:u8, start_date:u8, start_hour:u8, end_month:u8, end_date:u8, end_hour:u8, hour_modification:i8) -> Self {
+		let savings:DaylightSavings = DaylightSavings::new(start_month, start_date, start_hour, end_month, end_date, end_hour, hour_modification);
+		let modification:i8 = savings.get_modification(self.months, self.days, self.hours);
+		if modification != 0 {
+			self.seconds_since_y0 = ((self.seconds_since_y0 as i64) + (modification as i64 * SECONDS_PER_HOUR as i64)) as u64;
 		}
-		let is_leap_year:bool = year % 4 == 0;
+		self.parse_formatted();
+		self
+	}
+
+	/// Create a new formatter from raw date parts. Month and date should start at 0.
+	pub fn new_date_raw(year:u16, months:u8, days:u8, hours:u8, minutes:u8, seconds:u8) -> TimeFormatter {
+		let (year_u64, months_u64, days_u64, hours_u64, minutes_u64, seconds_u64) = (year as u64, months as u64, days as u64, hours as u64, minutes as u64, seconds as u64);
+		let leap_days:u64 = if year_u64 == 0 { 0 } else { (year_u64 - 1) / 4 + 1 }; // Leap years should be added once the year is over. 1 year passed means 1 leap year.
+
+		let days_in_years:u64 = year_u64 * 365 + leap_days;
+		let days_in_month:u64 = (0..months_u64).map(|month_index| if month_index == 1 { if year_u64 % 4 == 0 { 29 } else { 28 } } else { 30 + ((month_index + (if month_index > 6 { 0 } else { 1 })) % 2) }).sum::<u64>();
+		let seconds_since_y0:u64 = (((days_in_years + days_in_month + days_u64) * 24 + hours_u64) * 60 + minutes_u64) * 60 + seconds_u64;
+
+		TimeFormatter::new(seconds_since_y0)
+	}
+
+
+
+	/* PARSING METHODS */
+
+	/// Parse the formatted properties from the seconds since year 0.
+	fn parse_formatted(&mut self) {
+		let mut remaining_seconds:u64 = self.seconds_since_y0;
+
+		self.year = 0;
+		while Self::take_formatted_time(&mut remaining_seconds, SECONDS_PER_YEAR(self.is_leap_year())) {
+			self.year += 1;
+		}
+		let is_leap_year:bool = self.is_leap_year();
 		
-		let mut month_index:u64 = 0;
-		while Self::take_formatted_time(&mut remaining_seconds, SECONDS_PER_MONTH(month_index, is_leap_year)) {
-			month_index += 1;
+		self.months = 0;
+		while Self::take_formatted_time(&mut remaining_seconds, SECONDS_PER_MONTH(self.months as u64, is_leap_year)) {
+			self.months += 1;
 		}
 
-		let day_index:u64 = remaining_seconds / SECONDS_PER_DAY;
+		self.days = (remaining_seconds / SECONDS_PER_DAY) as u8;
 		remaining_seconds %= SECONDS_PER_DAY;
 
-		let hours:u64 = remaining_seconds / SECONDS_PER_HOUR;
+		self.hours = (remaining_seconds / SECONDS_PER_HOUR) as u8;
 		remaining_seconds %= SECONDS_PER_HOUR;
 
-		let minutes:u64 = remaining_seconds / SECONDS_PER_MINUTE;
+		self.minutes = (remaining_seconds / SECONDS_PER_MINUTE) as u8;
 		remaining_seconds %= SECONDS_PER_MINUTE;
 
-		let seconds:u64 = remaining_seconds;
-
-		TimeFormatter {
-			_seconds_since_y0: seconds_since_y0,
-			year: year as u16,
-			months: month_index as u8,
-			days: day_index as u8,
-			hours: hours as u8,
-			minutes: minutes as u8,
-			seconds: seconds as u8
-		}
+		self.seconds = remaining_seconds as u8;
 	}
 
 	/// Remove an amount from time from the remaining seconds. Returns false if there was not enough.
@@ -78,6 +107,11 @@ impl TimeFormatter {
 	/// Get the year.
 	pub fn year(&self) -> u16 {
 		self.year
+	}
+
+	/// Whether or not this is a leap year.
+	pub fn is_leap_year(&self) -> bool {
+		self.year % 4 == 0
 	}
 
 	/// Get the amount of months. Will return a value between 0 and 11 where 0 is the first month.
@@ -115,11 +149,36 @@ impl TimeFormatter {
 		self.seconds
 	}
 }
+impl PartialEq for TimeFormatter {
+	fn eq(&self, other:&Self) -> bool {
+		self.seconds_since_y0 == other.seconds_since_y0
+	}
+}
+impl PartialOrd for TimeFormatter {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		self.seconds_since_y0.partial_cmp(&other.seconds_since_y0)
+	}
+}
+impl Display for TimeFormatter {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{:04}-{:02}-{:02} {:02}:{:02}:{:02}", self.year(), self.month(), self.date(), self.hours(), self.minutes(), self.seconds())
+	}
+}
+impl Debug for TimeFormatter {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{:04}-{:02}-{:02} {:02}:{:02}:{:02}", self.year(), self.month(), self.date(), self.hours(), self.minutes(), self.seconds())
+	}
+}
 
 
 
 pub trait TimeFormattable {
 	fn seconds_since_y0(&self) -> u64;
+}
+impl TimeFormattable for TimeFormatter {
+	fn seconds_since_y0(&self) -> u64 {
+		self.seconds_since_y0
+	}
 }
 impl TimeFormattable for SystemTime {
 	fn seconds_since_y0(&self) -> u64 {
@@ -131,5 +190,98 @@ impl TimeFormattable for SystemTime {
 impl TimeFormattable for Duration {
 	fn seconds_since_y0(&self) -> u64 {
 		self.as_secs()
+	}
+}
+impl TimeFormattable for u64 {
+	fn seconds_since_y0(&self) -> u64 {
+		*self
+	}
+}
+
+
+
+struct DaylightSavings {
+	start_month_index:u8,
+	start_day_index:u8,
+	start_hour:u8,
+
+	end_month_index:u8,
+	end_day_index:u8,
+	end_hour:u8,
+
+	hour_modification:i8,
+	flipped:bool
+}
+impl DaylightSavings {
+
+	/// Create a new daylight savings set.
+	fn new(start_month:u8, start_date:u8, start_hour:u8, end_month:u8, end_date:u8, end_hour:u8, hour_modification:i8) -> DaylightSavings {
+		let flipped:bool = (end_month < start_month) || (end_month == start_month && end_date < start_date) || (end_month == start_month && end_date == start_date && end_hour < start_hour);
+		if !flipped {
+			DaylightSavings {
+				start_month_index: start_month - 1,
+				start_day_index: start_date - 1,
+				start_hour: start_hour,
+
+				end_month_index: end_month - 1,
+				end_day_index: end_date - 1,
+				end_hour: end_hour,
+
+				hour_modification,
+				flipped
+			}
+		} else {
+			DaylightSavings {
+				end_month_index: start_month - 1,
+				end_day_index: start_date - 1,
+				end_hour: start_hour,
+
+				start_month_index: end_month - 1,
+				start_day_index: end_date - 1,
+				start_hour: end_hour,
+
+				hour_modification,
+				flipped
+			}
+		}
+	}
+
+	/// Whether or not the modification should be applied.
+	fn should_apply_modification(&self, month_index:u8, day_index:u8, hour:u8) -> bool {
+
+		// Return false if it's too early.
+		if month_index < self.start_month_index {
+			return false;
+		} else if month_index == self.start_month_index {
+			if day_index < self.start_day_index {
+				return false;
+			} else if day_index == self.start_day_index && hour < self.start_hour {
+				return false;
+			}
+		}
+		
+		// Return false if it's too late.
+		if month_index > self.end_month_index {
+			return false;
+		} else if month_index == self.end_month_index {
+			if day_index > self.end_day_index {
+				return false;
+			} else if day_index == self.end_day_index && hour >= self.end_hour {
+				return false;
+			}
+		}
+
+		// No problems found, return true.
+		true
+	}
+
+	/// Get the modification for a specific date.
+	fn get_modification(&self, month_index:u8, day_index:u8, hour:u8) -> i8 {
+		let should_apply:bool = self.should_apply_modification(month_index, day_index, hour);
+		if should_apply != self.flipped {
+			self.hour_modification
+		} else {
+			0
+		}
 	}
 }
--- src/time_formatter_u.rs
+++ src/time_formatter_u.rs
@@ -1,26 +1,18 @@
 #[cfg(test)]
 mod tests {
+	use std::time::SystemTime;
 	use crate::TimeFormatter;
-	use std::time::Duration;
 
 
 
-	fn test_date(year:u64, months:u64, days:u64, hours:u64, minutes:u64, seconds:u64) {
-		let leap_years:u64 = if year == 0 { 0 } else { (year - 1) / 4 + 1 }; // Leap years should be added once the year is over. 1 year passed means 1 leap year.
-
-		// Convert date to seconds past y0.
-		let days_in_years:u64 = year * 365 + leap_years;
-		let days_in_months:u64 = (0..months).map(|month_index| if month_index == 1 { if year % 4 == 0 { 28 } else { 29 } } else { 30 + ((month_index + (if month_index > 6 { 0 } else { 1 })) % 2) }).sum();
-		let seconds_in_date:u64 = (((days_in_years + days_in_months + days) * 24 + hours) * 60 + minutes) * 60 + seconds;
-
-		// Validate time formatter concludes original date.
-		let formated_time:TimeFormatter = TimeFormatter::new(Duration::from_secs(seconds_in_date));
-		assert_eq!(formated_time.year() as u64, year);
-		assert_eq!(formated_time.months() as u64, months);
-		assert_eq!(formated_time.days() as u64, days);
-		assert_eq!(formated_time.hours() as u64, hours);
-		assert_eq!(formated_time.minutes() as u64, minutes);
-		assert_eq!(formated_time.seconds() as u64, seconds);
+	fn test_date(year:u16, months:u8, days:u8, hours:u8, minutes:u8, seconds:u8) {
+		let formated_time:TimeFormatter = TimeFormatter::new_date_raw(year, months, days, hours, minutes, seconds);
+		assert_eq!(formated_time.year(), year);
+		assert_eq!(formated_time.months(), months);
+		assert_eq!(formated_time.days(), days);
+		assert_eq!(formated_time.hours(), hours);
+		assert_eq!(formated_time.minutes(), minutes);
+		assert_eq!(formated_time.seconds(), seconds);
 	}
 
 
@@ -39,6 +31,7 @@ mod tests {
 	#[test]
 	fn test_time_day() {
 		for days in 0..31 {
+			println!("{days}");
 			test_date(0, 0, days, 8, 30, 15);
 		}
 	}
@@ -46,6 +39,7 @@ mod tests {
 	#[test]
 	fn test_time_day_month() {
 		for months in 0..12 {
+			println!("{months}");
 			test_date(0, months, 4, 8, 30, 15);
 		}
 	}
@@ -53,7 +47,24 @@ mod tests {
 	#[test]
 	fn test_time_day_month_year() {
 		for year in 0..2036 {
+			println!("{year}");
 			test_date(year, 8, 4, 8, 30, 15);
+		}
+	}
+
+	#[test]
+	fn test_daylight_savings() {
+		for (month, date, hour, should_mod) in [(3, 2, 0, false), (3, 2, 1, true), (4, 5, 6, true), (5, 1, 0, true), (5, 6, 0, true), (5, 6, 6, true), (5, 6, 7, false), (8, 1, 0, false)] {
+			let formatter:TimeFormatter = TimeFormatter::new_date(0, month, date, hour, 0, 0).with_daylight_saving_time(3, 2, 1, 5, 6, 7, 12);
+			assert_eq!(formatter.hours(), hour + if should_mod { 12 } else { 0 });
+		}
+	}
+
+	#[test]
+	fn test_daylight_savings_flipped() {
+		for (month, date, hour, should_mod) in [(3, 2, 0, false), (3, 2, 1, true), (4, 5, 6, true), (5, 1, 0, true), (5, 6, 0, true), (5, 6, 6, true), (5, 6, 7, false), (8, 1, 0, false)] {
+			let formatter:TimeFormatter = TimeFormatter::new_date(0, month, date, hour, 0, 0).with_daylight_saving_time(5, 6, 7, 3, 2, 1, 12);
+			assert_eq!(formatter.hours(), hour + if should_mod { 0 } else { 12 });
 		}
 	}
 }
